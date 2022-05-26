8f7c0d2f8d65ad87d625f9c2802c7836013d9531
すみそ <sumi@wattai.jp>
2022-05-26 19:50:42 +0900

    add: font追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,20 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    /*
+     ** Headers of the page
+     */
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
+        },
+      ],
+    },
+  },
+
   /*
    ** Global CSS
    */
