fe4e8a62ac87cddda489ef72cabda18b91b2dbef
すみそ <sumi@wattai.jp>
2022-05-27 00:57:54 +0900

    add: txtコンポーネント追加

---
+++ components/Elements/Txt.vue
@@ -0,0 +1,39 @@
+<template>
+  <component :is="tag" class="txt"><slot /></component>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: { type: String, default: 'span' },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables';
+
+.txt {
+  font-size: $font;
+  &.-xxsmall {
+    font-size: $font-xxsmall;
+  }
+  &.-xsmall {
+    font-size: $font-xsmall;
+  }
+  &.-small {
+    font-size: $font-small;
+  }
+  &.-large {
+    font-size: $font-large;
+  }
+  &.-xlarge {
+    font-size: $font-xlarge;
+  }
+
+  // 種類
+  &.-sub {
+    color: $color-gray-03;
+  }
+}
+</style>
