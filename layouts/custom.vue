a6b10663727abab361d4cbd5d54c7b02d7cd0921
すみそ <sumi@wattai.jp>
2022-05-26 19:51:24 +0900

    update: layout作り込み

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>
-    <TheHeader />
-    <slot />
-    <TheAppBar />
+  <div class="platform">
+    <TheHeader class="platform__header" />
+    <main class="platform__contents"><slot /></main>
+    <TheAppBar class="platform__footer" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables';
+
+.platform {
+  background: $color-gray-01;
+  max-width: map-get($breakpoints, medium);
+  margin: 0 auto;
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  position: relative;
+  background: $color-gray-09;
+  box-shadow: 0 0 32px rgba(0, 0, 0, 0.4);
+  &__footer {
+    position: sticky;
+    bottom: 0;
+    margin-top: auto;
+  }
+}
+</style>
