ba4aea636287bbbcc7df527447417e2a8b42bfb2
すみそ <sumi@wattai.jp>
2022-05-26 14:12:27 +0900

    add: 基本のスタイル追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  /*
+   ** Global CSS
+   */
+  css: ['@/assets/scss/style.scss'],
 })
