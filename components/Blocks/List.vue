0b323b42c3486ae7e4b2dd0523ac786fa8e390d5
すみそ <sumi@wattai.jp>
2022-05-27 01:42:23 +0900

    add: リストコンポーネント追加

---
+++ components/Blocks/List.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="list">
+    <slot />
+  </ul>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables';
+
+.list {
+  display: flex;
+  margin: $space;
+  gap: $space;
+  flex-wrap: wrap;
+  flex-direction: column;
+
+  &.-disc {
+    gap: 0;
+    li {
+      list-style: disc;
+      margin-left: 1.5em;
+    }
+  }
+  &.-decimal {
+    gap: 0;
+    li {
+      list-style: decimal;
+      margin-left: 1.5em;
+    }
+  }
+
+  &.-horizontal {
+    flex-wrap: wrap;
+    flex-direction: row;
+  }
+  &.-center {
+    justify-content: center;
+    align-items: center;
+  }
+  &.-right {
+    justify-content: end;
+  }
+}
+</style>
