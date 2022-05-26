f14db7137ceccdfcce69dad9e62273fcbdbe3d7a
すみそ <sumi@wattai.jp>
2022-05-26 22:06:15 +0900

    add: ページ内リンクの追加

---
+++ components/Blocks/IndexNav.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="index">
+    <ul>
+      <li>
+        <button>
+          <span>ナビ</span
+          ><span class="material-icons"> keyboard_arrow_down </span>
+        </button>
+      </li>
+      <li>
+        <button>
+          <span>ナビ</span
+          ><span class="material-icons"> keyboard_arrow_down </span>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables';
+.index ul {
+  background: $color-gray-10;
+  display: flex;
+  justify-content: space-between;
+  li {
+    flex: 1;
+    button {
+      display: flex;
+      padding: $space;
+      width: 100%;
+      box-sizing: border-box;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+}
+</style>
