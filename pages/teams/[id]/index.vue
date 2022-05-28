4eb8f7524018f4008b8441b228dd117c70d39f04
すみそ <sumi@wattai.jp>
2022-05-29 01:42:13 +0900

    add: チーム詳細追加

---
+++ pages/teams/[id]/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <BlocksLocalHeader>{{ title }}</BlocksLocalHeader>
+    <ProjectsTabsTeam />
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const title = 'チーム詳細'
+useHead({
+  title: title,
+})
+</script>
---
+++ pages/teams/[id]/tournaments.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <BlocksLocalHeader>{{ title }}</BlocksLocalHeader>
+    <ProjectsTabsTeam />
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const title = '開催した大会｜チーム詳細'
+useHead({
+  title: title,
+})
+</script>
