762bf9258c9a50275097d1bf9a8aaef83279af53
すみそ <sumi@wattai.jp>
2022-05-29 01:36:53 +0900

    update: 不要属性の削除

--- components/Projects/Tabs/Tournament.vue
+++ components/Projects/Tabs/Tournament.vue
@@ -1,6 +1,6 @@
 <template>
   <BlocksTab>
-    <li><nuxt-link to="/tournaments/1/" class="is-current">大会概要</nuxt-link></li>
+    <li><nuxt-link to="/tournaments/1/">大会概要</nuxt-link></li>
     <li><nuxt-link to="/tournaments/1/qualifying">予選</nuxt-link></li>
     <li><nuxt-link to="/tournaments/1/final">本戦</nuxt-link></li>
     <li><nuxt-link to="/tournaments/1/progress">進行表</nuxt-link></li>
