bf7081d7252c154d24a72e4ca9bd6b8343174875
すみそ <sumi@wattai.jp>
2022-05-29 18:27:08 +0900

    チームページのネスト

---
+++ pages/teams/[id].vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <BlocksHero
+      :name="team.name"
+      :background-image-url="team.background_image_url"
+    />
+    <ProjectsTabsTeam />
+    <NuxtPage :page-key="route.path" :team="team" />
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const team = (await $fetch('/api/teams/1')).data
+const title = team.name
+useHead({
+  title: title,
+})
+</script>
