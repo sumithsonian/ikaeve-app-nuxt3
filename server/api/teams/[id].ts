5c3f2825789d25f95590dc2c969706a3c4e89d30
すみそ <sumi@wattai.jp>
2022-05-30 03:55:01 +0900

    update: フォーマットかけ忘れ

--- server/api/teams/[id].ts
+++ server/api/teams/[id].ts
@@ -1,5 +1,7 @@
 export default defineEventHandler(async (event) => {
   const items = await $fetch('/api/teams')
-  const item = items.data.find((row) => row.id === Number(event.context.params.id))
+  const item = items.data.find(
+    (row) => row.id === Number(event.context.params.id),
+  )
   return { data: item }
 })
--- server/api/users/[id].ts
+++ server/api/users/[id].ts
@@ -1,5 +1,7 @@
 export default defineEventHandler(async (event) => {
   const items = await $fetch('/api/users')
-  const item = items.data.find((row) => row.id === Number(event.context.params.id))
+  const item = items.data.find(
+    (row) => row.id === Number(event.context.params.id),
+  )
   return { data: item }
 })
