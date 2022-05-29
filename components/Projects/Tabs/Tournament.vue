1def9a51b80aea2435cba89705cb7120cf6e3aad
すみそ <sumi@wattai.jp>
2022-05-30 04:09:34 +0900

    update: 本戦はfinalsとsが付くようなので修正

--- components/Projects/Tabs/Tournament.vue
+++ components/Projects/Tabs/Tournament.vue
@@ -2,7 +2,7 @@
   <BlocksTab>
     <li><nuxt-link to="/tournaments/1/" exact>大会概要</nuxt-link></li>
     <li><nuxt-link to="/tournaments/1/qualifying">予選</nuxt-link></li>
-    <li><nuxt-link to="/tournaments/1/final">本戦</nuxt-link></li>
+    <li><nuxt-link to="/tournaments/1/finals">本戦</nuxt-link></li>
     <li><nuxt-link to="/tournaments/1/progress">進行表</nuxt-link></li>
     <li><nuxt-link to="/tournaments/1/teams">参加チーム</nuxt-link></li>
   </BlocksTab>
