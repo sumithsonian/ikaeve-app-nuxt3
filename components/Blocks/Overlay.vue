11e4cd8beeaf3023137d46ebebee84c307ba4d23
すみそ <sumi@wattai.jp>
2022-05-31 22:07:10 +0900

    add: オーバーレイをコンポーネント化

---
+++ components/Blocks/Overlay.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="overlay"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables';
+
+.overlay {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba($color-black, 0.5);
+}
+</style>
