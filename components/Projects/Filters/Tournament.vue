d44405718c19521861b10f96779bf5358a2a1af2
すみそ <sumi@wattai.jp>
2022-06-02 21:54:33 +0900

    update: 並び順実装

--- components/Projects/Filters/Tournament.vue
+++ components/Projects/Filters/Tournament.vue
@@ -2,13 +2,23 @@
   <BlocksFilter>
     <li>
       <ElementsIcon> filter_alt </ElementsIcon>
-      <ElementsTag tag="button" class="-xsmall is-active">フィルタ</ElementsTag>
-      <ElementsTag tag="button" class="-xsmall">フィルタ</ElementsTag>
-      <ElementsTag tag="button" class="-xsmall">フィルタ</ElementsTag>
-      <ElementsTag tag="button" class="-xsmall">フィルタ</ElementsTag>
+      <ElementsTag tag="button" class="-xsmall is-active">全て</ElementsTag>
+      <ElementsTag tag="button" class="-xsmall">募集中</ElementsTag>
+      <ElementsTag tag="button" class="-xsmall">開催中</ElementsTag>
+      <ElementsTag tag="button" class="-xsmall">終了</ElementsTag>
     </li>
     <li>
-      <ElementsFormSelect> 新着順 </ElementsFormSelect>
+      <ElementsFormSelect :items="sorts" />
     </li>
   </BlocksFilter>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      sorts: ['新着順', '人気順'],
+    }
+  },
+}
+</script>
