60ae59f293b70ba8d76b5bd167ee30ddf8ebc5d4
すみそ <sumi@wattai.jp>
2022-06-03 01:09:56 +0900

    update: ドロワーメニューのスタイル調整

--- components/TheAppBar.vue
+++ components/TheAppBar.vue
@@ -39,57 +39,76 @@
       <div class="appNav__drawer__actions">
         <button @click="close"><ElementsIcon>close</ElementsIcon></button>
       </div>
-      <ul class="appNav__drawer__menu">
-        <li>
-          <nuxt-link to="/"
-            ><ElementsIcon> home </ElementsIcon> ホーム</nuxt-link
-          >
-        </li>
-        <li>
-          <nuxt-link to="/ranking/"
-            ><ElementsIcon> insights </ElementsIcon> ランキング</nuxt-link
-          >
-        </li>
-        <li>
-          <nuxt-link to="/tournaments/"
-            ><ElementsIcon> military_tech </ElementsIcon> 大会一覧</nuxt-link
-          >
-        </li>
-        <li>
-          <nuxt-link to="/teams/"
-            ><ElementsIcon> people </ElementsIcon> チーム/団体一覧</nuxt-link
-          >
-        </li>
-        <li>
-          <nuxt-link to="/players/"
-            ><ElementsIcon> person </ElementsIcon> ユーザー一覧</nuxt-link
-          >
-        </li>
-        <li><nuxt-link to="/about">イカイベとは？</nuxt-link></li>
-        <li><nuxt-link to="/terms">ご利用規約</nuxt-link></li>
-        <li><nuxt-link to="/privacy">プライバシーポリシー</nuxt-link></li>
-        <li>
-          <nuxt-link to="/tournaments/create"
-            ><ElementsIcon> add </ElementsIcon> 大会作成</nuxt-link
-          >
-        </li>
-        <li>
-          <nuxt-link to="/teams/create"
-            ><ElementsIcon> add </ElementsIcon> チーム/団体作成</nuxt-link
-          >
-        </li>
-        <li>
-          <nuxt-link to="/"
-            ><ElementsIcon> logout </ElementsIcon> ログアウト</nuxt-link
-          >
-        </li>
-        <li>
-          <nuxt-link to="/"
-            ><ElementsIcon> login </ElementsIcon>
-            新規会員登録/ログイン</nuxt-link
-          >
-        </li>
-      </ul>
+      <div class="appNav__drawer__menu">
+        <ul>
+          <li>
+            <nuxt-link to="/"
+              ><ElementsIcon> home </ElementsIcon> ホーム</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link to="/ranking/"
+              ><ElementsIcon> insights </ElementsIcon> ランキング</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link to="/tournaments/"
+              ><ElementsIcon> military_tech </ElementsIcon> 大会一覧</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link to="/teams/"
+              ><ElementsIcon> people </ElementsIcon> チーム/団体一覧</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link to="/players/"
+              ><ElementsIcon> person </ElementsIcon> ユーザー一覧</nuxt-link
+            >
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <nuxt-link class="-small" to="/about"
+              ><ElementsIcon> link </ElementsIcon>イカイベとは？</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link class="-small" to="/terms"
+              ><ElementsIcon> link </ElementsIcon>ご利用規約</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link class="-small" to="/privacy"
+              ><ElementsIcon> link </ElementsIcon>
+              プライバシーポリシー</nuxt-link
+            >
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <nuxt-link to="/tournaments/create"
+              ><ElementsIcon> add </ElementsIcon> 大会作成</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link to="/teams/create"
+              ><ElementsIcon> add </ElementsIcon> チーム/団体作成</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link to="/"
+              ><ElementsIcon> logout </ElementsIcon> ログアウト</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link to="/"
+              ><ElementsIcon> login </ElementsIcon>
+              新規会員登録/ログイン</nuxt-link
+            >
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -143,16 +162,27 @@ export default {
     right: 0;
     bottom: 0;
     display: flex;
-    ul {
+    &__menu {
       background: $color-gray-09;
       height: 100vh;
-      li {
-        a {
-          display: flex;
-          align-items: center;
-          padding: $space;
-          span {
-            margin-right: $space;
+      padding: $space;
+      ul {
+        border-bottom: 1px solid $color-gray-07;
+        &:last-child {
+          border: none;
+        }
+        li {
+          a {
+            display: flex;
+            align-items: center;
+            padding: $space 0;
+            span {
+              margin-right: $space;
+            }
+            &.-small {
+              font-size: $font-xsmall;
+              padding: $space-small 0;
+            }
           }
         }
       }
