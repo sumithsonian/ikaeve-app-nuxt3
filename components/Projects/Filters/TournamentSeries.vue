883552636b93bc39da39afb9a19f87b46a6584fd
すみそ <sumi@wattai.jp>
2022-06-08 12:22:01 +0900

    update: シリーズ一覧実装

---
+++ components/Projects/Filters/TournamentSeries.vue
@@ -0,0 +1,38 @@
+<template>
+  <BlocksFilter>
+    <li>
+      <ElementsIcon> filter_alt </ElementsIcon>
+      <ElementsTag tag="button" class="-xsmall is-active">全て</ElementsTag>
+    </li>
+    <li>
+      <ElementsFormSelect v-model="localValue.sort" :items="sorts" />
+    </li>
+  </BlocksFilter>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: { type: Object, default: null },
+  },
+  data() {
+    return {
+      sorts: [
+        { value: null, name: '新着順' },
+        { value: 'popular', name: '人気順' },
+        { value: 'tier', name: 'ティア順' },
+      ],
+    }
+  },
+  computed: {
+    localValue: {
+      get() {
+        return this.modelValue
+      },
+      set(value) {
+        this.$emit('update:modelValue', value)
+      },
+    },
+  },
+}
+</script>
---
+++ pages/tournament-series/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <BlocksLocalHeader>{{ title }}</BlocksLocalHeader>
+    <ProjectsFiltersTournamentSeries v-model="query" />
+    <ProjectsCardsTournamentSeries :items="tournamentSeries" />
+    <ProjectsPagination />
+  </div>
+</template>
+
+<script setup>
+const title = '大会シリーズ一覧'
+useHead({
+  title: title,
+})
+
+const route = useRoute()
+const query = {
+  sort: route.query.sort || null,
+  page: route.query.page || null,
+}
+
+const tournamentSeries = (await $fetch('/api/tournament-series')).data
+</script>
