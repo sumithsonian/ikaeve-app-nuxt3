7d4e530679a9b3e73981dd005810af4af7ab0197
すみそ <sumi@wattai.jp>
2022-06-17 21:48:53 +0900

    update: 大会の出し分け要素変更

--- pages/tournaments/[id].vue
+++ pages/tournaments/[id].vue
@@ -12,70 +12,72 @@
     </template>
     <NuxtPage :tournament="tournament" />
     <template v-if="!route.path.includes('edit')">
-      <BlocksInformation v-if="tournament.status === 'recruiting'">
-        <BlocksList class="-horizontal -center">
-          <li>
-            <ElementsButton class="-primary"
-              >大会へのエントリーリンクをコピーする</ElementsButton
-            >
-          </li>
-        </BlocksList>
-        <BlocksList class="-horizontal -center">
-          <li>
-            <ElementsButton
-              @click="entryTournamentModalState = true"
-              class="-primary"
-              >大会にエントリーする</ElementsButton
-            >
-          </li>
-        </BlocksList>
-        <BlocksList class="-horizontal -center">
-          <li>
-            <ElementsButton
-              @click="cancelTournamentModalState = true"
-              class="-primary"
-              >大会のエントリーを取り消す</ElementsButton
-            >
-          </li>
-        </BlocksList>
-        <BlocksParagraph class="-center"
-          ><ElementsText class="-small"
-            >現在のエントリ数：12 / 128</ElementsText
-          ></BlocksParagraph
-        >
-      </BlocksInformation>
-      <BlocksInformation v-else-if="tournament.status === 'recruitment_closed'">
-        <BlocksParagraph class="-center"
-          >募集チームが揃いました</BlocksParagraph
-        >
-        <BlocksList class="-horizontal -center">
-          <li>
-            <ElementsButton
-              :to="`/tournaments/${tournament.id}/qualifying`"
-              class="-primary"
-              >組み合わせの決定に進む</ElementsButton
-            >
-          </li>
-        </BlocksList>
-        <BlocksParagraph class="-center"
-          >募集チームが定員に達しませんでした</BlocksParagraph
-        >
-        <BlocksList class="-horizontal -center">
-          <li>
-            <ElementsButton
-              class="-primary"
-              :to="`/tournaments/${tournament.id}/edit`"
-              >期限を延ばす</ElementsButton
-            >
-          </li>
-          <li>
-            <ElementsButton
-              class="-primary"
-              :to="`/tournaments/${tournament.id}/edit`"
-              >大会を中止する</ElementsButton
-            >
-          </li>
-        </BlocksList>
+      <BlocksInformation>
+        <template v-if="tournament.status === 'recruiting'">
+          <BlocksList class="-horizontal -center">
+            <li>
+              <ElementsButton class="-primary"
+                >大会へのエントリーリンクをコピーする</ElementsButton
+              >
+            </li>
+          </BlocksList>
+          <BlocksList class="-horizontal -center">
+            <li>
+              <ElementsButton
+                @click="entryTournamentModalState = true"
+                class="-primary"
+                >大会にエントリーする</ElementsButton
+              >
+            </li>
+          </BlocksList>
+          <BlocksList class="-horizontal -center">
+            <li>
+              <ElementsButton
+                @click="cancelTournamentModalState = true"
+                class="-primary"
+                >大会のエントリーを取り消す</ElementsButton
+              >
+            </li>
+          </BlocksList>
+          <BlocksParagraph class="-center"
+            ><ElementsText class="-small"
+              >現在のエントリ数：12 / 128</ElementsText
+            ></BlocksParagraph
+          >
+        </template>
+        <template v-else-if="tournament.status === 'recruitment_closed'">
+          <BlocksParagraph class="-center"
+            >募集チームが揃いました</BlocksParagraph
+          >
+          <BlocksList class="-horizontal -center">
+            <li>
+              <ElementsButton
+                :to="`/tournaments/${tournament.id}/qualifying`"
+                class="-primary"
+                >組み合わせの決定に進む</ElementsButton
+              >
+            </li>
+          </BlocksList>
+          <BlocksParagraph class="-center"
+            >募集チームが定員に達しませんでした</BlocksParagraph
+          >
+          <BlocksList class="-horizontal -center">
+            <li>
+              <ElementsButton
+                class="-primary"
+                :to="`/tournaments/${tournament.id}/edit`"
+                >期限を延ばす</ElementsButton
+              >
+            </li>
+            <li>
+              <ElementsButton
+                class="-primary"
+                :to="`/tournaments/${tournament.id}/edit`"
+                >大会を中止する</ElementsButton
+              >
+            </li>
+          </BlocksList>
+        </template>
       </BlocksInformation>
     </template>
   </div>
