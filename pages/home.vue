ce02b90aefed336179453d2eff0ba75b9cd7573a
すみそ <sumi@wattai.jp>
2022-06-20 21:52:54 +0900

    add: ログイン後のhomeを追加

---
+++ pages/home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <BlocksHeading>主催大会</BlocksHeading>
+    <ProjectsCardsTournament :items="tournaments.data" class="-scroll" />
+    <BlocksHeading>主催大会シリーズ</BlocksHeading>
+    <ProjectsCardsTournamentSeries
+      :items="tournamentSeries.data"
+      class="-scroll"
+    />
+    <BlocksHeading>エントリ中の大会</BlocksHeading>
+    <ProjectsCardsTournament :items="tournaments2.data" class="-scroll" />
+  </div>
+</template>
+
+<script setup>
+const { $fetch2 } = useNuxtApp()
+const me = useMeState()
+
+const { data: tournaments } = await $fetch2(`/api/users/${me.value.id}/tournaments`, {
+  per_page: 10,
+})
+const { data: tournaments2 } = await $fetch2(`/api/users/${me.value.id}/tournaments`, {
+  per_page: 10,
+})
+const { data: tournamentSeries } = await $fetch2(`/api/users/${me.value.id}/tournament-series`, {
+  per_page: 10,
+})
+
+useHead({
+  title: 'イカイベ',
+  titleTemplate: null,
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,37 @@
 <template>
   <div>
-    <template v-if="isLoggedIn">
-      <BlocksHeading>主催大会</BlocksHeading>
-      <ProjectsCardsTournament :items="tournaments.data" class="-scroll" />
-      <BlocksHeading>主催大会シリーズ</BlocksHeading>
-      <ProjectsCardsTournamentSeries
-        :items="tournamentSeries.data"
-        class="-scroll"
-      />
-      <BlocksHeading>エントリ中の大会</BlocksHeading>
-      <ProjectsCardsTournament :items="tournaments.data" class="-scroll" />
-    </template>
-    <template v-else>
-      <TheHero />
-      <BlocksHeading>募集中の大会</BlocksHeading>
-      <ProjectsCardsTournament :items="tournaments.data" class="-scroll" />
-      <BlocksList class="-center">
-        <li>
-          <ElementsButton to="/tournaments/?status=recruiting"
-            >全ての大会をみる</ElementsButton
-          >
-        </li>
-      </BlocksList>
-      <BlocksHeading>開催中の大会</BlocksHeading>
-      <ProjectsCardsTournament :items="tournaments.data" class="-scroll" />
-      <BlocksList class="-center">
-        <li>
-          <ElementsButton to="/tournaments/?status=ongoing"
-            >全ての大会をみる</ElementsButton
-          >
-        </li>
-      </BlocksList>
-    </template>
+    <TheHero />
+    <BlocksHeading>募集中の大会</BlocksHeading>
+    <ProjectsCardsTournament :items="tournaments.data" class="-scroll" />
+    <BlocksList class="-center">
+      <li>
+        <ElementsButton to="/tournaments/?status=recruiting"
+          >全ての大会をみる</ElementsButton
+        >
+      </li>
+    </BlocksList>
+    <BlocksHeading>開催中の大会</BlocksHeading>
+    <ProjectsCardsTournament :items="tournaments2.data" class="-scroll" />
+    <BlocksList class="-center">
+      <li>
+        <ElementsButton to="/tournaments/?status=ongoing"
+          >全ての大会をみる</ElementsButton
+        >
+      </li>
+    </BlocksList>
   </div>
 </template>
 
 <script setup>
 const { $fetch2 } = useNuxtApp()
-const { data: tournaments } = await $fetch2('/api/tournaments', { per_page: 5 })
-const { data: tournamentSeries } = await $fetch2('/api/tournament-series', { per_page: 5 })
-const isLoggedIn = useIsLoggedInState()
+
+const { data: tournaments } = await $fetch2('/api/tournaments', {
+  per_page: 10,
+})
+const { data: tournaments2 } = await $fetch2('/api/tournaments', {
+  per_page: 10,
+})
+
 useHead({
   title: 'イカイベ',
   titleTemplate: null,
