63edf91fcf6a9eea4b7768f4e16a1224395df0d1
すみそ <sumi@wattai.jp>
2022-06-20 19:19:28 +0900

    update: ティア修正

--- server/api/tournaments/index.get.ts
+++ server/api/tournaments/index.get.ts
@@ -67,7 +67,7 @@ export default defineEventHandler((event) => {
         },
       },
       questions: ['イキゴミ', '好きなブキ'],
-      tear: (i - 1) % 4 + 1,
+      tear: ((i - 1) % 4) + 1,
       distribution_point: 192,
       is_owner: i === 1,
       is_entered: i === 1,
