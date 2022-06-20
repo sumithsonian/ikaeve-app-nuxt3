9a743cded079c14eeb61d37738f6783cc9938638
すみそ <sumi@wattai.jp>
2022-06-20 17:55:55 +0900

    bugfix: findだと1件しかとれないのでfilterに変更

--- server/api/users/[id]/teams.ts
+++ server/api/users/[id]/teams.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const items = await $fetch('/api/teams')
-  const item = items.data.find(
+  const item = items.data.filter(
     (row) => row.owner_user.id === Number(event.context.params.id),
   )
   return { data: item }
--- server/api/users/[id]/tournament-series.ts
+++ server/api/users/[id]/tournament-series.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const items = await $fetch('/api/tournament-series')
-  const item = items.data.find(
+  const item = items.data.filter(
     (row) => row.team.owner_user.id === Number(event.context.params.id),
   )
   return { data: item }
