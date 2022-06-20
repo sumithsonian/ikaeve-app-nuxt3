a573769719c6c0faf33b94e52ef651ab8819ea42
すみそ <sumi@wattai.jp>
2022-06-20 16:37:36 +0900

    add: fetchのラッパー関数追加

--- pages/index.vue
+++ pages/index.vue
@@ -2,19 +2,19 @@
   <div>
     <template v-if="isLoggedIn">
       <BlocksHeading>主催大会</BlocksHeading>
-      <ProjectsCardsTournament :items="tournaments" class="-scroll" />
+      <ProjectsCardsTournament :items="tournaments.data" class="-scroll" />
       <BlocksHeading>主催大会シリーズ</BlocksHeading>
       <ProjectsCardsTournamentSeries
-        :items="tournamentSeries"
+        :items="tournamentSeries.data"
         class="-scroll"
       />
       <BlocksHeading>エントリ中の大会</BlocksHeading>
-      <ProjectsCardsTournament :items="tournaments" class="-scroll" />
+      <ProjectsCardsTournament :items="tournaments.data" class="-scroll" />
     </template>
     <template v-else>
       <TheHero />
       <BlocksHeading>募集中の大会</BlocksHeading>
-      <ProjectsCardsTournament :items="tournaments" class="-scroll" />
+      <ProjectsCardsTournament :items="tournaments.data" class="-scroll" />
       <BlocksList class="-center">
         <li>
           <ElementsButton to="/tournaments/?status=recruiting"
@@ -23,7 +23,7 @@
         </li>
       </BlocksList>
       <BlocksHeading>開催中の大会</BlocksHeading>
-      <ProjectsCardsTournament :items="tournaments" class="-scroll" />
+      <ProjectsCardsTournament :items="tournaments.data" class="-scroll" />
       <BlocksList class="-center">
         <li>
           <ElementsButton to="/tournaments/?status=ongoing"
@@ -36,12 +36,12 @@
 </template>
 
 <script setup>
+const { $fetch2 } = useNuxtApp()
+const { data: tournaments } = await $fetch2('/api/tournaments', { per_page: 5 })
+const { data: tournamentSeries } = await $fetch2('/api/tournament-series', { per_page: 5 })
+const isLoggedIn = useIsLoggedInState()
 useHead({
   title: 'イカイベ',
   titleTemplate: null,
 })
-
-const tournaments = (await $fetch('/api/tournaments')).data
-const tournamentSeries = (await $fetch('/api/tournament-series')).data
-const isLoggedIn = useIsLoggedInState()
 </script>
