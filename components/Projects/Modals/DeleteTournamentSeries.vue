4ed649b88b6212545ff65041fb899b6a10f7d70d
すみそ <sumi@wattai.jp>
2022-06-30 20:25:48 +0900

    add: 大会シリーズ削除モーダル追加

---
+++ components/Projects/Modals/DeleteTournamentSeries.vue
@@ -0,0 +1,38 @@
+<template>
+  <BlocksModal v-if="isActive" @close-from-child="close">
+    <template #heading>大会シリーズを削除する</template>
+    <template #contents>
+      <BlocksParagraph class="-center">大会シリーズを削除しますか？</BlocksParagraph>
+      <BlocksList class="-center">
+        <li>
+          <ElementsButton class="-primary -large">削除する</ElementsButton>
+        </li>
+        <li>
+          <ElementsButton @click="close" class="-large"
+            >キャンセルする</ElementsButton
+          >
+        </li>
+      </BlocksList>
+      <BlocksParagraph class="-center"
+        ><ElementsText class="-sub -xsmall"
+          >※一度削除すると復元できません。<br />
+          削除される場合はお気を付け下さい。</ElementsText
+        ></BlocksParagraph
+      >
+    </template>
+  </BlocksModal>
+</template>
+
+<script setup>
+const isActive = useDeleteTournamentSeriesModalState()
+</script>
+
+<script>
+export default {
+  methods: {
+    close() {
+      this.isActive = false
+    },
+  },
+}
+</script>
