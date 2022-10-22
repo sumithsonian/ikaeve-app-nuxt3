ab8ae6692bb38b9239918175476e784a7198753c
すみそ <sumi@wattai.jp>
2022-10-22 12:19:44 +0900

    update: リネームに合わせてurl変更

--- server/api/events/[id]/index.ts
+++ server/api/events/[id]/index.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async (event) => {
-  const items = await $fetch('/api/tournaments')
+  const items = await $fetch('/api/events')
   const item = items.data.find(
     (row) => row.id === Number(event.context.params.id),
   )
--- server/api/tournaments/[id]/tournaments.ts
+++ server/api/tournaments/[id]/tournaments.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async (event) => {
-  const items = await $fetch('/api/tournaments')
+  const items = await $fetch('/api/events')
   const item = items.data.filter(
     (row) => row.series.id === Number(event.context.params.id),
   )
